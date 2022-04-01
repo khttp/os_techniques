2d42eea58682d513c887e553d126938afd43a11a
abdelrahmankhattab9999@gmail.com <abdelrahmankhattab9999@gmail.com>
2022-04-01 15:29:20 +0200

    simple refactoring

--- challenge1.py
+++ challenge1.py
@@ -5,15 +5,13 @@ from matplotlib import use
 
 account=input('entre the account and i will give u enfo about u \n--> ')
 
-
-at_char=account.find('@')
-username=account[:at_char]
+username=account[:account.find('@')]
 print(f'the user name is : {username}')
 
-dot_char=account.find('.')
-company_name=account[at_char+1:dot_char]
+company_name=account[account.find('@')+1:account.find('.')]
 print(f'the company name is : {company_name}')
 
-org_name=account[dot_char+1:]
+org_name=account[account.find('.')+1:]
 print(f'the organiztion name is : {org_name}')
+
 
